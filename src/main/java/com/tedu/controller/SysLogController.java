bc604c5ce7e8828f2d6599314002d8472a19dde4
yangxn-626 <18369903959@163.com>
2020-04-25 22:34:01 +0800

    第四次

--- src/main/java/com/tedu/controller/SysLogController.java
+++ src/main/java/com/tedu/controller/SysLogController.java
@@ -2,6 +2,6 @@ package com.tedu.controller;
 
 public class SysLogController {
     public static void main(String[] args) {
-        System.out.println("这个ma");
+        System.out.println("这个");
     }
 }
