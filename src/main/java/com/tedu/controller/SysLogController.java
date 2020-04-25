b7578f3596353bd07e826df8ff11c92d4b499734
yangxn-626 <18369903959@163.com>
2020-04-25 22:18:21 +0800

    modify main

--- src/main/java/com/tedu/controller/SysLogController.java
+++ src/main/java/com/tedu/controller/SysLogController.java
@@ -2,6 +2,6 @@ package com.tedu.controller;
 
 public class SysLogController {
     public static void main(String[] args) {
-        System.out.println("s这个");
+        System.out.println("这个");
     }
 }
